739745c1b594d79bffb5302ce9adb490e942017c
dinaarh <rahmawatidina0404@gmail.com>
2026-02-18 08:46:43 +0700

    Upload folder PBF lengkap P1 dan P2

---
+++ P1/praktikum/my-app/src/pages/index.tsx
@@ -0,0 +1,39 @@
+import Head from 'next/head'
+import Link from 'next/link'
+
+export default function Home() {
+  return (
+    <div style={{
+      padding: '2rem',
+      fontFamily: 'Arial, sans-serif'
+    }}>
+      <Head>
+        <title>Praktikum Next.js</title>
+      </Head>
+
+      <h1 style={{ color: '#1F497D' }}>
+        Praktikum Next.js Pages Router
+      </h1>
+      <br />
+      <p>Mahasiswa D4 Pengembangan Web</p>
+      <br />
+
+      {/* navigasi */}
+      <Link
+        href='/about'
+        style={{
+          display: 'inline-block',
+          marginTop: '1rem',
+          padding: '10px 20px',
+          backgroundColor: '#2E75B6',
+          color: '#fff',
+          textDecoration: 'none',
+          borderRadius: '6px',
+          fontSize: '16px'
+        }}
+      >
+        Lihat Profil Saya
+      </Link>
+    </div>
+  )
+}
