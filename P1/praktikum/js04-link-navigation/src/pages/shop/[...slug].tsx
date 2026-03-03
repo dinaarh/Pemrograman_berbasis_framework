6b46657fc6afb6f03fd311d4f305ed7ff78f83b7
dinaarh <rahmawatidina0404@gmail.com>
2026-03-04 01:24:34 +0700

    3_ Modifikasi Isi file  […slug].tsx

---
+++ P1/praktikum/js04-link-navigation/src/pages/shop/[...slug].tsx
@@ -0,0 +1,15 @@
+import { useRouter } from 'next/router';
+
+const halamanToko = () => {
+  const Router = useRouter();
+  console.log(Router);
+  // const { query } = useRouter();
+
+  return (
+    <div>
+      <h1>Halaman Toko</h1>
+    </div>
+  );
+};
+
+export default halamanToko;
