90b1149b3be3eccbf757f2337a90583cdd1dba44
dinaarh <rahmawatidina0404@gmail.com>
2026-03-10 21:39:05 +0700

    9_Modifikasi index.tsx pada folder auth/login/

--- P1/praktikum/js05-styling/src/pages/views/auth/login/index.tsx
+++ P1/praktikum/js05-styling/src/pages/views/auth/login/index.tsx
@@ -22,7 +22,10 @@ const halamanLogin = () => {
   return (
     <div className={styles.login}>
       <div style={{ padding: '20px' }}>
-        <h1>Halaman Login</h1>
+            <h1 className='text-3xl font-bold text-blue-600'>
+            Halaman Login
+            </h1>
+
 
         {/* Form sederhana */}
         <div>
@@ -44,7 +47,7 @@ const halamanLogin = () => {
       </div>
 
       {/* Navigasi Imperatif: Login -> Produk */}
-      <button onClick={() => handlerLogin()}>
+      <button onClick={() => handlerLogin()} className='mt-4 px-6 py-2 bg-blue-500 text-white rounded hover:bg-blue-700'>
         Login ke Halaman Produk
       </button>
 
