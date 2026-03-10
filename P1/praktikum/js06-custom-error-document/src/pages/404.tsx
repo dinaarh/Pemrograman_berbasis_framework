0c7f1e1432b331468e2e2b8bc84b22f32365a061
dinaarh <rahmawatidina0404@gmail.com>
2026-03-10 23:31:45 +0700

    Langkah 6 – Menampilkan Gambar dari Folder Public

--- P1/praktikum/js06-custom-error-document/src/pages/404.tsx
+++ P1/praktikum/js06-custom-error-document/src/pages/404.tsx
@@ -3,6 +3,12 @@ import styles from '@/styles/404.module.scss';
 const Custom404 = () => {
   return (
     <div className={styles.error}>
+      <img
+        src="/page-not-found.png"
+        alt="404 - Halaman Tidak Ditemukan"
+        className={styles.error__image}
+      />
+
       <h1>404 - Halaman Tidak Ditemukan</h1>
       <p>Maaf, halaman yang Anda cari tidak ada.</p>
     </div>
