ee48726a92855b4f728235f39b879e1e4f1485e4
dinaarh <rahmawatidina0404@gmail.com>
2026-03-10 20:38:16 +0700

    5_bModifikasi login.tsx pada folder pages/auth

--- P1/praktikum/js05-styling/src/pages/auth/login.tsx
+++ P1/praktikum/js05-styling/src/pages/auth/login.tsx
@@ -1,58 +1,10 @@
-import Link from 'next/link';
-import { useRouter } from 'next/router';
-import { useState } from 'react';
-import styles from './login.module.css';
+import TampilanLogin from '../views/auth/login';
 
 const halamanLogin = () => {
-  const { push } = useRouter();
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-
-  const handlerLogin = () => {
-    // Simulasi: jika username dan password diisi, navigasi ke produk
-    if (username && password) {
-      localStorage.setItem('isLogin', 'true');
-      push('/produk');
-    } else {
-      alert('Harap isi username dan password!');
-    }
-  };
-
   return (
-    <div className={styles.login}>
-      <div style={{ padding: '20px' }}>
-        <h1>Halaman Login</h1>
-
-        {/* Form sederhana */}
-        <div>
-          <input
-            type='text'
-            placeholder='Username'
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            style={{ display: 'block', marginBottom: '10px' }}
-          />
-          <input
-            type='password'
-            placeholder='Password'
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            style={{ display: 'block', marginBottom: '10px' }}
-          />
-        </div>
-      </div>
-
-      {/* Navigasi Imperatif: Login -> Produk */}
-      <button onClick={() => handlerLogin()}>
-        Login ke Halaman Produk
-      </button>
-
-      <br /><br />
-
-      {/* Navigasi Deklaratif: Login <-> Register */}
-      <p>Belum punya akun?</p>
-      <Link href='/auth/register'>Ke Halaman Register</Link>
-    </div>
+    <>
+      <TampilanLogin />
+    </>
   );
 };
 
