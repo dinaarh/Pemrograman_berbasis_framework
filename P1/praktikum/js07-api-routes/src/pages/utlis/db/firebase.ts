8f4cc8c20aca805d536edef5cd514b716452f8e5
dinaarh <rahmawatidina0404@gmail.com>
2026-04-06 23:12:34 +0700

    Langkah 8 – Konfigurasi Firebase

--- P1/praktikum/js07-api-routes/src/pages/utlis/db/firebase.ts
+++ P1/praktikum/js07-api-routes/src/pages/utlis/db/firebase.ts
@@ -5,13 +5,16 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDaIkdVgasxyRCPVZmN4Cc_BzZVoDhEC7c",
-  authDomain: "framework-next-c25be.firebaseapp.com",
-  projectId: "framework-next-c25be",
-  storageBucket: "framework-next-c25be.firebasestorage.app",
-  messagingSenderId: "346994646098",
-  appId: "1:346994646098:web:c6476feeed323a5ae1d38b"
+  apiKey:            process.env.FIREBASE_API_KEY,
+  authDomain:        process.env.FIREBASE_AUTH_DOMAIN,
+  projectId:         process.env.FIREBASE_PROJECT_ID,
+  storageBucket:     process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId:             process.env.FIREBASE_APP_ID,
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+export default app;
