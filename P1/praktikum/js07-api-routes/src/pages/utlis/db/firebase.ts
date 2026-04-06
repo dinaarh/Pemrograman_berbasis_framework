b859bf7e3da6090ed0c23d6af99ab4751aae926b
dinaarh <rahmawatidina0404@gmail.com>
2026-04-06 22:54:22 +0700

    Langkah 6 – Install Firebase

---
+++ P1/praktikum/js07-api-routes/src/pages/utlis/db/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDaIkdVgasxyRCPVZmN4Cc_BzZVoDhEC7c",
+  authDomain: "framework-next-c25be.firebaseapp.com",
+  projectId: "framework-next-c25be",
+  storageBucket: "framework-next-c25be.firebasestorage.app",
+  messagingSenderId: "346994646098",
+  appId: "1:346994646098:web:c6476feeed323a5ae1d38b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
