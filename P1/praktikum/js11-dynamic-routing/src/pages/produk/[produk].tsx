3550dbf1fc4d788423458a905a0ebd3b554bbdb7
dinaarh <rahmawatidina0404@gmail.com>
2026-04-14 01:47:43 +0700

    Implementasi SSR

--- P1/praktikum/js11-dynamic-routing/src/pages/produk/[produk].tsx
+++ P1/praktikum/js11-dynamic-routing/src/pages/produk/[produk].tsx
@@ -2,21 +2,41 @@ import fetcher from '@/utils/swr/fetcher';
 import { useRouter } from 'next/router';
 import useSWR from 'swr';
 import DetailProduk from '../../views/DetailProduct';
+import { ProductType } from '@/types/Product.type';
 
-const HalamanProduk = () => {
-  const { query } = useRouter();
-  const { data, error, isLoading } = useSWR(
-    query.produk ? `/api/produk/${query.produk}` : null,
-    fetcher
-  );
+const HalamanProduk = ({ product }: { product: ProductType }) => {
+//   const { query } = useRouter();
+//   const { data, error, isLoading } = useSWR(
+//     query.produk ? `/api/produk/${query.produk}` : null,
+//     fetcher
+//   );
 
-  if (isLoading) return <div>Memuat data produk...</div>;
-  if (error) return <div>Gagal memuat data.</div>;
+//   if (isLoading) return <div>Memuat data produk...</div>;
+//   if (error) return <div>Gagal memuat data.</div>;
 
-  return (
+//   return (
+//     <div>
+//       <DetailProduk products={data?.data} />
+//     </div>
+//   );
+// };
+return (
     <div>
-      <DetailProduk products={data?.data} />
+      <DetailProduk products={product} />
     </div>
   );
 };
 export default HalamanProduk;
+
+// {/digunakan server-side rendering/}
+export async function getServerSideProps({ params }: { params: { produk: string } }) {
+  const res = await fetch(
+    `http://localhost:3000/api/produk/${params?.produk}`
+  );
+  const respone = await res.json();
+  return {
+    props: {
+      product: respone.data ?? null,
+    },
+  };
+}
