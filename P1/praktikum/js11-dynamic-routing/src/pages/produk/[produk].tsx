fe25fb1f5849dbe935c1f9001daeea83dacd640d
dinaarh <rahmawatidina0404@gmail.com>
2026-04-14 01:29:28 +0700

    Modifikasi file [product].tsx

--- P1/praktikum/js11-dynamic-routing/src/pages/produk/[produk].tsx
+++ P1/praktikum/js11-dynamic-routing/src/pages/produk/[produk].tsx
@@ -1,18 +1,22 @@
-import fetcher from "@/utils/swr/fetcher";
-import { useRouter } from "next/router";
-import useSWR from "swr";
+import fetcher from '@/utils/swr/fetcher';
+import { useRouter } from 'next/router';
+import useSWR from 'swr';
+import DetailProduk from '../../views/DetailProduct';
 
 const HalamanProduk = () => {
-  // const Router = useRouter();
-  // console.log(Router);
   const { query } = useRouter();
-  const{data,error,isLoading} = useSWR(`/api/products/${query.produk}`,fetcher);
+  const { data, error, isLoading } = useSWR(
+    query.produk ? `/api/produk/${query.produk}` : null,
+    fetcher
+  );
+
+  if (isLoading) return <div>Memuat data produk...</div>;
+  if (error) return <div>Gagal memuat data.</div>;
+
   return (
     <div>
-      <h1>Halaman Produk</h1>
-      <p>Produk: {query.produk}</p>
+      <DetailProduk products={data?.data} />
     </div>
   );
 };
-
 export default HalamanProduk;
