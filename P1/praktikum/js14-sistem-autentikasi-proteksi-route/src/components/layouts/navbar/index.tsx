76f6d075004f539ada1aa6d308a76853674fd0d5
dinaarh <rahmawatidina0404@gmail.com>
2026-04-14 11:19:40 +0700

    menangkap data pada session

--- P1/praktikum/js14-sistem-autentikasi-proteksi-route/src/components/layouts/navbar/index.tsx
+++ P1/praktikum/js14-sistem-autentikasi-proteksi-route/src/components/layouts/navbar/index.tsx
@@ -2,12 +2,17 @@ import styles from './navbar.module.css';
 import { signIn, signOut, useSession } from 'next-auth/react'
 
 const Navbar = () => {
+  const {data}:any = useSession()
   return (
     <div className={styles.navbar}>
       <div className="big">
         <h1>Navbar Component</h1>
       </div>
-          <button onClick={() => signIn()}>Sign In</button>
+      {data ? (
+        <button onClick={() => signOut()}>Sign Out</button>
+      ) : (
+        <button onClick={() => signIn()}>Sign In</button>
+      )}
     </div>
   );
 };
