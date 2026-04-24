489b1758ee91585d2150816841827411ded73412
dinaarh <rahmawatidina0404@gmail.com>
2026-04-24 11:55:10 +0700

    Tambahkan form inputan pada file index.tsx register

--- P1/praktikum/js15-authregister-database-integration/src/views/auth/register/index.tsx
+++ P1/praktikum/js15-authregister-database-integration/src/views/auth/register/index.tsx
@@ -5,7 +5,50 @@ const TampilanRegister = () => {
   return (
     <div className={style.register}>
       <h1 className={style.register__title}>Halaman Register</h1>
-      <Link href='/auth/login'>Ke Halaman Login</Link>
+      <div className={style.register__form}>
+        <form action=''>
+          {/* Field Email */}
+          <div className={style.register__form__item}>
+            <label htmlFor='email'
+              className={style.register__form__item__label}>
+              Email
+            </label>
+            <input type='email' id='email' name='email'
+              placeholder='Email'
+              className={style.register__form__item__input} />
+          </div>
+          {/* Field Fullname */}
+          <div className={style.register__form__item}>
+            <label htmlFor='Fullname'
+              className={style.register__form__item__label}>
+              Fullname
+            </label>
+            <input type='text' id='Fullname' name='Fullname'
+              placeholder='Fullname'
+              className={style.register__form__item__input} />
+          </div>
+          {/* Field Password */}
+          <div className={style.register__form__item}>
+            <label htmlFor='Password'
+              className={style.register__form__item__label}>
+              Password
+            </label>
+            <input type='password' id='Password' name='Password'
+              placeholder='Password'
+              className={style.register__form__item__input} />
+          </div>
+          {/* Button Register */}
+          <button type='submit'
+            className={style.register__form__item__button}>
+            Register
+          </button>
+        </form>
+        <br />
+        <p className={style.register__form__item__text}>
+          Sudah punya akun?
+          <Link href='/auth/login'>Ke Halaman Login</Link>
+        </p>
+      </div>
     </div>
   );
 };
