6b6fdf55afa6e7d8a8e00703c013d276fc0d1acc
dinaarh <rahmawatidina0404@gmail.com>
2026-04-24 12:21:56 +0700

    Modifikasi index.tsx pada folder register

--- P1/praktikum/js15-authregister-database-integration/src/views/auth/register/index.tsx
+++ P1/praktikum/js15-authregister-database-integration/src/views/auth/register/index.tsx
@@ -1,12 +1,43 @@
 import Link from 'next/link';
 import style from '../../auth/register/register.module.scss';
+import { useState } from 'react';
+import { useRouter } from 'next/router';
 
 const TampilanRegister = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const { push } = useRouter();
+  const [error, setError] = useState('');
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    setError('');
+    setIsLoading(true);
+    event.preventDefault();
+    const form = event.currentTarget;
+    const formData = new FormData(event.currentTarget);
+    const email = formData.get('email') as string;
+    const fullname = formData.get('Fullname') as string;
+    const password = formData.get('Password') as string;
+    const response = await fetch('/api/register', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ email, fullname, password }),
+    });
+    if (response.status === 200) {
+      form.reset();
+      setIsLoading(false);
+      push('/auth/login');
+    } else {
+      setIsLoading(false);
+      setError(response.status === 400 ? 'Email already exists' : 'An error occurred');
+    }
+  };
+
   return (
     <div className={style.register}>
+      {error && <p className={style.register__error}>{error}</p>}
       <h1 className={style.register__title}>Halaman Register</h1>
       <div className={style.register__form}>
-        <form action=''>
+        <form onSubmit={handleSubmit}>
           {/* Field Email */}
           <div className={style.register__form__item}>
             <label htmlFor='email'
@@ -37,10 +68,11 @@ const TampilanRegister = () => {
               placeholder='Password'
               className={style.register__form__item__input} />
           </div>
-          {/* Button Register */}
+           {/* Button Register */}
           <button type='submit'
-            className={style.register__form__item__button}>
-            Register
+            className={style.register__form__item__button}
+            disabled={isLoading}>
+            {isLoading ? 'Loading...' : 'Register'}
           </button>
         </form>
         <br />
